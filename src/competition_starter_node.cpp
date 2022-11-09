1db333641d190723e474ced84cecb7476837ead9
Michael Carlstrom <rmc170@case.edu>
2022-11-09 15:31:52 -0800

    push to partners

--- src/competition_starter_node.cpp
+++ src/competition_starter_node.cpp
@@ -12,16 +12,63 @@
 #include "tf2_ros/transform_listener.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include "geometry_msgs/TransformStamped.h"
+#include "sensor_msgs/JointState.h"
+#include <iostream>
+#include "ur_kinematics/ur_kin.h"
+#include "trajectory_msgs/JointTrajectory.h"
 
 
 std::vector<osrf_gear::Order> order_vector;
+sensor_msgs::JointState joint_states;
 ros::ServiceClient materialLocations;
 std::vector<std::vector<osrf_gear::Model>> camera_data = std::vector<std::vector<osrf_gear::Model>>(10);
 ros::ServiceClient gml ;
 tf2_ros::Buffer tfBuffer;
 
+ros::Publisher trajectoryPub;
+
+// std::vector<std::vector<double>>T_pose[4][4], T_des[4][4] = std::vector<std::vector<double>>(4)(4);
+// Instantiate variables for use with the kinematic system.
+double T_pose[4][4], T_des[4][4];
+double q_pose[6], q_des[8][6];
+
+// std::vector<std::vector<double>> q_des[8][6] = std::vector<std::vector<double>>(8)(6);
+trajectory_msgs::JointTrajectory desired;
+
 void orderCallback(const osrf_gear::Order msg){
     order_vector.push_back(msg);
+}
+void jointCallback(const sensor_msgs::JointState j){
+    joint_states = j;
+
+    std::string str;
+    std::string str2 = "hi";
+    for (std::string s : j.name)
+    {
+        str = str + " " + s + " ";
+    }
+
+    // q_pose.insert(
+    //     q_pose.end(),
+    //     joint_states.position.begin() + 1,
+    //     joint_states.position.end());
+    memcpy(q_pose, &joint_states.position[0] + 1, 6);
+    ur_kinematics::forward((double *)&q_pose, (double *)&T_pose);
+
+    //ROS_INFO_STREAM(str2);
+    ROS_INFO_STREAM_THROTTLE(10, str);
+
+
+
+    /*
+    float T[4][4] = {{0.0, -1.0, 0.0, X_POS}, \
+{0.0, 0.0, 1.0, Y_POS}, \
+{-1.0, 0.0, 0.0 , Z_POS}, \
+{0.0, 0.0, 0.0, 1.0}};;
+*/
+    
+    //fflush(stdout);
+    //throw std::runtime_error("hi");
 }
 
 void printOrderModelPose(){
@@ -46,33 +93,218 @@ void printOrderModelPose(){
                    
                     for(osrf_gear::Model model : camera_data[binNum]){
                         if(strstr(productType.c_str(),model.type.c_str())){
+                            // ROS_INFO("HERE- 1");
+                            // fflush(stdout);
                             geometry_msgs::Point point = model.pose.position;
                             ROS_WARN("name:= %s x:=%f y:=%f z:=%f",model.type.c_str(),point.x,point.y,point.z);
+
+                            // ROS_INFO("HERE 0");
+                            // fflush(stdout);
+                            /*
+                            ROS_WARN("T des %lf", T_des.at(0).at(3));
+                            ROS_INFO("YIPPE");
+                            fflush(stdout);
+                            ROS_WARN("Point X%lf", point.x);
+                            fflush(stdout);
+                            */
+                            
+                            ROS_INFO("HERE 8");
+                            fflush(stdout);
 
                             std::string frame = "logical_camera_"+su.unit_id+ "_frame";
                             geometry_msgs::TransformStamped tfStamped;
                             try {
-                                tfStamped = tfBuffer.lookupTransform("arm1_vacuum_gripper_link",frame,
+                                tfStamped = tfBuffer.lookupTransform("arm1_base_link",frame,
                                 ros::Time(0.0), ros::Duration(1.0));
                                 ROS_DEBUG("Transform to [%s] from [%s]", tfStamped.header.frame_id.c_str(),
                                 tfStamped.child_frame_id.c_str());
                             } catch (tf2::TransformException &ex) {
                                 ROS_ERROR("%s", ex.what());
                             }
+                            ROS_INFO("here 9");
 
                             geometry_msgs::PoseStamped part_pose, goal_pose;
                             part_pose.pose = model.pose;
-                           
+
+                            
                             tf2::doTransform(part_pose, goal_pose, tfStamped);
 
                             // Add height to the goal pose.
-                            goal_pose.pose.position.z += 0.10; // 10 cm above the part
-                            // Tell the end effector to rotate 90 degrees around the y-axis (in quaternions...).
-                            goal_pose.pose.orientation.w = 0.707;
+                           goal_pose.pose.position.z += 0.10; // 10 cm above the part
+                          //  Tell the end effector to rotate 90 degrees around the y-axis (in quaternions...).
+                           goal_pose.pose.orientation.w = 0.707;
                             goal_pose.pose.orientation.x = 0.0;
-                            goal_pose.pose.orientation.y = 0.707;
-                            goal_pose.pose.orientation.z = 0.0;
-                            tf2::doTransform(part_pose, goal_pose, tfStamped);
+                           goal_pose.pose.orientation.y = 0.707;
+                           goal_pose.pose.orientation.z = 0.0;
+
+                            // Add height to the goal pose.
+                            // goal_pose.pose.position.z += 0.10; // 10 cm above the part
+                            // // Tell the end effector to rotate 90 degrees around the y-axis (in quaternions...).
+                            // goal_pose.pose.orientation.w = 0.707;
+                            // goal_pose.pose.orientation.x = 0.0;
+                            // goal_pose.pose.orientation.y = 0.707;
+                            // goal_pose.pose.orientation.z = 0.0;
+
+
+
+                           // double x = goal_pose.pose.position.x;
+                          //  double y = goal_pose.pose.position.y;
+                           // double z = goal_pose.pose.position.z;
+
+                            double x = point.x;
+                            double y = point.y;
+                            double z = point.z;
+
+                            ROS_INFO("x: %lf, y:%lf, z:%lf", x, y, z);
+
+                            ROS_INFO("LAB 6 stuff");
+                            T_des[0][3] = x;
+                            // ROS_INFO("HERE 0.1");
+                            // fflush(stdout);
+                            T_des[1][3] = y;
+                            // ROS_INFO("HERE 0.2");
+                            // fflush(stdout);
+                            T_des[2][3] = z+ 0.3;
+                            // ROS_INFO("HERE 0.3");
+                            // fflush(stdout);
+                            T_des[3][3] = 1;
+                            // ROS_INFO("HERE 0.4");
+                            // fflush(stdout);
+
+                            // ROS_INFO("HERE 1");
+                            // fflush(stdout);
+
+                            // The orientation of the end effector so that the end effector is down.
+                            // T_des[0][0] = 0.0; T_des[0][1] = -1.0; T_des[0][2] = 0.0;
+                            // T_des[1][0] = 0.0; T_des[1][1] = 0.0; T_des[1][2] = 1.0;
+                            // T_des[2][0] = -1.0; T_des[2][1] = 0.0; T_des[2][2] = 0.0;
+                            // T_des[3][0] = 0.0; T_des[3][1] = 0.0; T_des[3][2] = 0.0;
+
+                            // T_des[0][0] = 1.0; T_des[0][1] = 0.0; T_des[0][2] = 0.0;
+                            // T_des[1][0] = 0.0; T_des[1][1] = 1.0; T_des[1][2] = 0.0;
+                            // T_des[2][0] = 0.0; T_des[2][1] = 0.0; T_des[2][2] = 1.0;
+                            // T_des[3][0] = 0.0; T_des[3][1] = 0.0; T_des[3][2] = 0.0;
+
+                            int num_sols = ur_kinematics::inverse((double *)&T_des, (double *)&q_des);
+                            ROS_INFO("INVS SOLs: %d", num_sols);
+
+                            if(num_sols==0) {
+                                continue;
+                            }
+                            ROS_INFO("INVS SOLs: %d", num_sols);
+                            trajectory_msgs::JointTrajectory joint_trajectory;
+
+                            // ROS_INFO("HERE 2");
+                            // fflush(stdout);
+
+                            // Fill out the joint trajectory header.
+                            // Each joint trajectory should have an non-monotonically increasing sequence number.
+                            static int count = 0;
+                            joint_trajectory.header.seq = count++;
+                            joint_trajectory.header.stamp = ros::Time::now(); // When was this message created.
+                            joint_trajectory.header.frame_id = "/world"; // Frame in which this is specified
+
+                            // ROS_INFO("HERE 3");
+                            // fflush(stdout);
+
+                            // Set the names of the joints being used. All must be present.
+                            joint_trajectory.joint_names.clear();
+                            joint_trajectory.joint_names.push_back("linear_arm_actuator_joint");
+                            joint_trajectory.joint_names.push_back("shoulder_pan_joint");
+                            joint_trajectory.joint_names.push_back("shoulder_lift_joint");
+                            joint_trajectory.joint_names.push_back("elbow_joint");
+                            joint_trajectory.joint_names.push_back("wrist_1_joint");
+                            joint_trajectory.joint_names.push_back("wrist_2_joint");
+                            joint_trajectory.joint_names.push_back("wrist_3_joint");
+
+                            // ROS_INFO("HERE 4");
+                            // fflush(stdout);
+
+                            // Set a start and end point.
+                            joint_trajectory.points.resize(2);
+
+                            // ROS_INFO("HERE 5");
+                            // fflush(stdout);
+
+                            //joint_states = joints;
+                            // Set the start point to the current position of the joints from joint_states.
+                            joint_trajectory.points[0].positions.resize(joint_trajectory.joint_names.size());
+                            for (int indy = 0; indy < joint_trajectory.joint_names.size(); indy++) {
+                                for (int indz = 0; indz < joint_states.name.size(); indz++) {
+                                    if (joint_trajectory.joint_names[indy] == joint_states.name[indz]) {
+                                        joint_trajectory.points[0].positions[indy] = joint_states.position[indz];
+                                        break;
+                                    }
+                                }
+                            }
+
+                            // ROS_INFO("HERE 6");
+                            // fflush(stdout);
+
+                                                        // When to start (immediately upon receipt).
+                            joint_trajectory.points[0].time_from_start = ros::Duration(0.0);
+                            // ROS_INFO("HERE 6.1s");
+                            // fflush(stdout);
+                            // Must select which of the num_sols solutions to use. Just start with the first.
+                            int q_des_indx = 0;
+                            // Set the end point for the movement
+                            joint_trajectory.points[1].positions.resize(joint_trajectory.joint_names.size());
+                            // Set the linear_arm_actuator_joint from joint_states as it is not part of the inverse kinematics solution.
+                            //  ROS_INFO("HERE 6.5");
+                            // fflush(stdout);
+
+
+                            joint_states.position.at(1);
+                            // ROS_INFO("HERE 6.55");
+                            // fflush(stdout);
+
+                            (joint_trajectory.points.at(1)).positions.at(0) = joint_states.position.at(1);
+                            // The actuators are commanded in an odd order, enter the joint positions in the correct positions
+                            // ROS_INFO("HERE 6.6");
+                            // fflush(stdout);
+                            for (int indy = 0; indy < 6; indy++) {
+                                joint_trajectory.points[1].positions[indy + 1] = q_des[q_des_indx][indy];
+                            }
+                            // ROS_INFO("HERE 6.7");
+                            // fflush(stdout);
+
+                            // ROS_INFO("HERE 7");
+                            // fflush(stdout);
+                            // How long to take for the movement.
+                            joint_trajectory.points[1].time_from_start = ros::Duration(1.0);
+
+                            trajectoryPub.publish(joint_trajectory);
+
+                            // ROS_INFO("HERE 8");
+                            // fflush(stdout);
+
+                            // std::string frame = "logical_camera_"+su.unit_id+ "_frame";
+                            // geometry_msgs::TransformStamped tfStamped;
+                            // try {
+                            //     tfStamped = tfBuffer.lookupTransform("arm1_vacuum_gripper_link",frame,
+                            //     ros::Time(0.0), ros::Duration(1.0));
+                            //     ROS_DEBUG("Transform to [%s] from [%s]", tfStamped.header.frame_id.c_str(),
+                            //     tfStamped.child_frame_id.c_str());
+                            // } catch (tf2::TransformException &ex) {
+                            //     ROS_ERROR("%s", ex.what());
+                            // }
+
+                            // geometry_msgs::PoseStamped part_pose, goal_pose;
+                            // part_pose.pose = model.pose;
+                           
+                            // tf2::doTransform(part_pose, goal_pose, tfStamped);
+
+                            // // Add height to the goal pose.
+                            // goal_pose.pose.position.z += 0.10; // 10 cm above the part
+                            // // Tell the end effector to rotate 90 degrees around the y-axis (in quaternions...).
+                            // goal_pose.pose.orientation.w = 0.707;
+                            // goal_pose.pose.orientation.x = 0.0;
+                            // goal_pose.pose.orientation.y = 0.707;
+                            // goal_pose.pose.orientation.z = 0.0;
+                            // tf2::doTransform(part_pose, goal_pose, tfStamped);
+
+
+                            // throw std::runtime_error("hi");
                         }
                     }
                 }
@@ -84,7 +316,20 @@ void printOrderModelPose(){
 
 int main(int argc, char* argv[]){
 
-    
+    //T_pose.resize(4, std::vector<double>(4));
+    //T_des.resize(4, std::vector<double>(4));
+    //q_des.resize(8, std::vector<double>(6));
+
+    ROS_INFO("T_desing");
+    fflush(stdout);
+    //T_des.at(0).at(3) = 0.1;
+   // T_des[1][3] = 1.1;
+   // T_des[2][3] = 2.1;
+   //T_des[3][3] = 3.1;
+    //ros::Duration(1.0).sleep();
+    ROS_INFO("T_desed");
+    fflush(stdout);
+
     ros::init(argc, argv, "lab5"); 
     ros::NodeHandle n;
     tf2_ros::TransformListener tfListener(tfBuffer);
@@ -109,8 +354,10 @@ int main(int argc, char* argv[]){
         ROS_INFO("Competition started!");
     }
 
+    trajectoryPub = n.advertise<trajectory_msgs::JointTrajectory>("/ariac/arm1/arm/command",1000);
     
-    ros::Subscriber sub = n.subscribe<osrf_gear::Order>("/ariac/orders", 1000,orderCallback);
+    
+    ros::Subscriber sub = n.subscribe<osrf_gear::Order>("/ariac/orders", 1000, orderCallback);
     std::vector<ros::Subscriber> binCameras = std::vector<ros::Subscriber>(6);
     std::vector<ros::Subscriber> agvCameras= std::vector<ros::Subscriber>(2);
     std::vector<ros::Subscriber> qualityCameras= std::vector<ros::Subscriber>(2);
@@ -156,5 +403,9 @@ int main(int argc, char* argv[]){
             }
         });
     }
+
+    ros::Subscriber jointSub = n.subscribe<sensor_msgs::JointState>("/ariac/arm1/joint_states", 1000,jointCallback);
+    
+
     ros::spin();    
 }
